700dd5c85ae9e43d4e8d786c88826d3a307f1b4d
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-11-03 23:22:31 +0100

    update package.swift update travis remove swiftlint warning

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
   name: "DependencyInjectorObjectMapper",
   dependencies: [
     .Package(url: "https://github.com/Digipolitan/dependency-injector.git", versions: Version(2, 0, 0)..<Version(3, 0, 0)),
-    .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", versions: Version(2, 0, 0)..<Version(3, 0, 0))
+    .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", versions: Version(3, 0, 0)..<Version(4, 0, 0))
   ],
   exclude: [
     "Tests",
